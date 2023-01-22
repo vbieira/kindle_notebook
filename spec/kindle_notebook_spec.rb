b0a27e2b594d3b0b6cbf72dda1f1003fe4bc8073
Bianca Vieira <vbieira@outlook.com>
2023-01-22 15:44:56 -0300

    create kindle_notebook gem

---
+++ spec/kindle_notebook_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe KindleNotebook do
+  it "has a version number" do
+    expect(KindleNotebook::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
